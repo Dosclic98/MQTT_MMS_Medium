3f0d0dcdfc7b2c064a70de73a056bbb024d9f79d
DAVIDE SAVARRO <davidesavarro98@gmail.com>
2023-08-01 18:07:40 +0200

    Started implementation of operation moduled

---
+++ src/operation/client/MmsClientOperation.cc
@@ -0,0 +1,29 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "MmsClientOperation.h"
+
+using namespace inet;
+
+IOperator* MmsClientOperation::getOperatorOwner() {
+	if(auto* oper = dynamic_cast<MmsClientOperator*>(operatorOwner)) { return oper; }
+	else throw std::invalid_argument("operatorOwner must be a subtype or MmsClientOperator");
+}
+
+void MmsClientOperation::setOperatorOwner(IOperator* operatorOwner) {
+	MmsClientOperator* oper = dynamic_cast<MmsClientOperator*>(operatorOwner);
+	if(oper) {this->operatorOwner = operatorOwner;}
+	else { throw std::invalid_argument("operatorOwner must be a subtype or MmsClientOperator"); }
+}
