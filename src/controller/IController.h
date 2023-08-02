8b223ad797f8f05af40f8b9bfdb2d19abc6f9f36
DAVIDE SAVARRO <davidesavarro98@gmail.com>
2023-08-02 17:59:50 +0200

    Started implementation of the Controller modules

---
+++ src/controller/IController.h
@@ -0,0 +1,33 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef __TX_MEDIUM_EXP_ICONTROLLER_H_
+#define __TX_MEDIUM_EXP_ICONTROLLER_H_
+
+#include <omnetpp.h>
+
+using namespace omnetpp;
+
+/**
+ * TODO - Generated class
+ */
+class IController : public cSimpleModule
+{
+  protected:
+    virtual void initialize() override;
+    virtual void handleMessage(cMessage *msg) override;
+};
+
+#endif
