695b3449603ddb23234df4648e60d0ec0ceb6130
DAVIDE SAVARRO <davidesavarro98@gmail.com>
2023-08-08 13:32:26 +0200

    Started MmsServerController implementation

---
+++ src/controller/server/MmsServerController.h
@@ -0,0 +1,41 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef __TX_MEDIUM_EXP_MMSSERVERCONTROLLER_H_
+#define __TX_MEDIUM_EXP_MMSSERVERCONTROLLER_H_
+
+#include "../IController.h";
+
+namespace inet {
+
+/**
+ * TODO - Generated class
+ */
+class MmsServerController : public cSimpleModule, public IController {
+  protected:
+	MmsServerController();
+	virtual ~MmsServerController();
+
+    virtual void initialize() override;
+    virtual void handleMessage(cMessage *msg) override;
+
+    virtual void next(MmsMessage* msg = nullptr) override;
+    virtual void propagate(IOperation* op) override;
+    virtual void evalRes(IResult* res) override;
+};
+
+} // namespace inet
+
+#endif
