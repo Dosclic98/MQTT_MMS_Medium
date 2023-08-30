938457e3ab2767adb71c21acb4c591ee1e8cc5c2
DAVIDE SAVARRO <davidesavarro98@gmail.com>
2023-08-30 17:29:12 +0200

    Started implementaton of the MmsAttackerController (STUB)

---
+++ src/controller/attacker/MmsAttackerController.cc
@@ -0,0 +1,40 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "MmsAttackerController.h"
+
+using namespace inet;
+
+Define_Module(MmsAttackerController);
+
+void MmsAttackerController::initialize() {
+    // TODO - Generated method body
+}
+
+void MmsAttackerController::handleMessage(cMessage *msg) {
+    // TODO - Generated method body
+}
+
+void MmsAttackerController::next(Packet* msg) {
+
+}
+
+void MmsAttackerController::propagate(IOperation* op) {
+
+}
+
+void MmsAttackerController::evalRes(IResult* res) {
+
+}
