86153933f462cfac540cf1173d0375d40caf91e0
Davide Savarro <davidesavarro98@gmail.com>
2024-09-10 18:02:32 +0200

    Started implementation of the PingEvilOperator

---
+++ src/operator/attacker/PingEvilOperator.cc
@@ -0,0 +1,58 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "PingEvilOperator.h"
+
+#define INIT_SOCKET 1
+
+Define_Module(PingEvilOperator);
+
+PingEvilOperator::~PingEvilOperator() {
+    cancelAndDelete(timer);
+    socketMap.deleteSockets();
+}
+
+
+void PingEvilOperator::initialize() {
+    timer = new cMessage("Initialize socket", INIT_SOCKET);
+    scheduleAt(simTime(), timer);
+}
+
+void PingEvilOperator::handleMessage(cMessage *msg) {
+    // TODO This must be transfered within a new ping send
+    if(msg->getKind() == INIT_SOCKET) {
+        currentSocket = nullptr;
+        currentSocket = new Ipv4Socket(gate("socketOut"));
+        socketMap.addSocket(currentSocket);
+        currentSocket->bind(icmp, L3Address());
+        currentSocket->setCallback(this);
+    }
+}
+
+void PingApp::socketClosed(INetworkSocket *socket)
+{
+    if (socket == currentSocket)
+        currentSocket = nullptr;
+    delete socketMap.removeSocket(socket);
+}
+
+void PingEvilOperator::propagate(IResult* res) {
+    emit(this->resPubSig, res);
+}
+
+void PingEvilOperator::propagate(Packet* msg) {
+    emit(this->msgPubSig, msg);
+}
+
