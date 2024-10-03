2ea1b9ef324190f26ac5e7bfa7bcf3c2fada00f8
Davide Savarro <davidesavarro98@gmail.com>
2024-10-03 17:38:47 +0200

    Implemented factory for the SendHttpTcpDisconnectAkt operation

---
+++ src/operation/factory/event/concrete/SendHttpTcpDisconnectAtkFactory.cc
@@ -0,0 +1,39 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "SendHttpTcpDisconnectAtkFactory.h"
+#include "../../../../controller/attacker/HttpAttackerController.h"
+#include "../../../attacker/concrete/SendHttpTcpDisconnectAtk.h"
+
+using namespace inet;
+
+void SendHttpTcpDisconnectAtkFactory::build(omnetpp::cEvent* event) {
+    HttpAttackerController* controller = check_and_cast<HttpAttackerController*>(this->controller);
+
+    // Store the current IP address to which the attacker was connected
+    controller->saveCurrentIp();
+    SendHttpTcpDisconnectAtk* atkOp = new SendHttpTcpDisconnectAtk();
+    controller->propagate(atkOp);
+}
+
+
+SendHttpTcpDisconnectAtkFactory::SendHttpTcpDisconnectAtkFactory(HttpAttackerController* controller) {
+    this->controller = controller;
+}
+
+SendHttpTcpDisconnectAtkFactory::~SendHttpTcpDisconnectAtkFactory() {
+    // TODO Auto-generated destructor stub
+}
+
---
+++ src/operation/factory/event/concrete/SendHttpTcpDisconnectAtkFactory.h
@@ -0,0 +1,35 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef OPERATION_FACTORY_EVENT_CONCRETE_SENDHTTPTCPDISCONNECTATKFACTORY_H_
+#define OPERATION_FACTORY_EVENT_CONCRETE_SENDHTTPTCPDISCONNECTATKFACTORY_H_
+
+#include "../EventOperationFactory.h"
+
+namespace inet {
+
+class HttpAttackerController;
+
+class SendHttpTcpDisconnectAtkFactory : public EventOperationFactory {
+public:
+    virtual void build(omnetpp::cEvent* event) override;
+
+    SendHttpTcpDisconnectAtkFactory(HttpAttackerController* controller);
+    virtual ~SendHttpTcpDisconnectAtkFactory();
+};
+
+}; // namespace inet
+
+#endif /* OPERATION_FACTORY_EVENT_CONCRETE_SENDHTTPTCPDISCONNECTATKFACTORY_H_ */
