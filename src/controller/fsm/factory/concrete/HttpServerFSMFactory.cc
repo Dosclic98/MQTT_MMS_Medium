8f723249deebd67a6cad1ab9554d1ead02243727
Davide Savarro <davidesavarro98@gmail.com>
2024-10-07 22:54:28 +0200

    Started implementation of the HttpServerFSMFactory

---
+++ src/controller/fsm/factory/concrete/HttpServerFSMFactory.cc
@@ -0,0 +1,39 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include "HttpServerFSMFactory.h"
+#include "../../state/concrete/OpState.h"
+#include "../../transition/concrete/EventTransition.h"
+#include "../../transition/concrete/PacketTransition.h"
+#include "../../../server/HttpServerController.h"
+
+using namespace inet;
+
+HttpServerFSMFactory::HttpServerFSMFactory(HttpServerController* controller) {
+    this->controller = controller;
+}
+
+HttpServerFSMFactory::~HttpServerFSMFactory() {
+    // TODO Auto-generated destructor stub
+}
+
+IFSM* HttpServerFSMFactory::build() {
+    HttpServerController* controller = static_cast<HttpServerController*>(this->controller);
+    OpState* operativeState = new OpState("OPERATIVE");
+
+    std::vector<std::shared_ptr<ITransition>> operativeTransitions;
+    //std::shared_ptr<ITransition> operativeOk = std::make_shared<PacketTransition>();
+}
+
